8f89c8958de6d28ef08e5987926d1bd014dfef4e
Ganesh Tiwari <ganesht049@gmail.com>
2025-07-12 14:27:30 +0530

    feat: added a script

--- zx-scripts/getting-started.mjs
+++ zx-scripts/getting-started.mjs
@@ -1,11 +1,26 @@
 #!/usr/bin/env zx
+import fs from "fs";
 
-await $`cat package.json | grep name`;
+const output = await $`cat package.json | grep name`;
+console.log(output.stdout);
 
 const branch = await $`git branch --show-current`;
-await $`dep deploy --branch=${branch}`;
+console.log(branch.stdout);
 
 await Promise.all([$`sleep 1; echo 1`, $`sleep 2; echo 2`, $`sleep 3; echo 3`]);
 
-const name = "foo bar";
-await $`mkdir /tmp/${name}`;
+const name = "foo-bar";
+const full_path = `/tmp/${name}`;
+const is_already_present = fs.existsSync(full_path);
+
+if (!is_already_present) {
+  console.log("Directory is nonexistent, creating...");
+  await $`mkdir /tmp/${name}`;
+  console.log("Created...");
+} else {
+  console.log("Directory is already present, I will first delete it");
+  const rm_output = await $`rm -rf ${full_path}`;
+  console.log("Removed..., Now creating...");
+  await $`mkdir /tmp/${name}`;
+  console.log("Created...");
+}
